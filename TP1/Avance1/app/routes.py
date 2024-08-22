daa8f6bec11df98d81178f5915207dacf1e6adbd
Diego Bolaños <diego.bolanosvillalobos@ucr.ac.cr>
2024-08-21 22:09:25 -0600

    Added routes for new features

--- TP1/Avance1/app/routes.py
+++ TP1/Avance1/app/routes.py
@@ -4,3 +4,15 @@ from flask import current_app as app
 @app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
+
+@app.route('/feature1.html', methods=['GET'])
+def render_feature_1():
+    return render_template("feature1.html")
+
+@app.route('/feature2.html', methods=['GET'])
+def render_feature_2():
+    return render_template("feature2.html")
+
+@app.route('/feature3.html', methods=['GET'])
+def render_feature_3():
+    return render_template("feature3.html")
