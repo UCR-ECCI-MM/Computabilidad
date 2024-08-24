8f555399980190ed7144d2c968a35a8de202770f
JosePabloMoraC <84686084+JosePabloMoraC@users.noreply.github.com>
2024-08-23 19:41:18 -0600

    Added barplot of feature 2

--- TP1/Avance1/app/routes.py
+++ TP1/Avance1/app/routes.py
@@ -5,6 +5,8 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from io import BytesIO
+import base64
 
 
 """ EXAMPLE DATA """
@@ -12,7 +14,21 @@ dictionary = {}
 with open("app/data/example.json", "r") as data:
     dictionary = ast.literal_eval(data.read())
 
-# example data with  <information- category> tag counts.
+# Example data with  <information- category> tag counts:
+inf_category_counts = {
+    'Treatments and Therapies': 1000,
+    'Patient Handouts': 900,
+    'Encyclopedia': 600,
+    'Start Here': 700,
+    'Find an Expert': 800,
+    'Reference Desk': 500,
+    'Journal Articles': 400,
+    'Symptoms': 100,
+    'Health Check Tools': 300,
+    'Related Issues': 200,
+    'Learn More': 10, 
+    'Find an expert': 40
+}
 
 @app.route('/', methods=['GET'])
 def index():
@@ -24,8 +40,31 @@ def render_feature_1():
 
 @app.route('/feature2.html', methods=['GET'])
 def render_feature_2():
+    # cast the `inf_category_counts` dictionary to a dataFrame
+    df_category_counts = pd.DataFrame(list(inf_category_counts.items()), columns=['Category', 'Count'])
+    # get the 10 most popular categories
+    popular_category_counts = df_category_counts.sort_values(by='Count', ascending=False).head(10)
 
-    return render_template("feature2.html")
+    # create a horizontal barplot
+    #plt.figure(figsize=(2, 4))
+    plt.figure(figsize=(6, 6))
+    sns.barplot(x='Count', y='Category', data=popular_category_counts, orient='h')
+    #plt.title('10 Most Popular Information Categories')
+    plt.xlabel('Count')
+    plt.ylabel('Category')
+    plt.tight_layout() 
+
+    # save the barplot to a temporary buffer 
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    #buffer.seek(0)
+
+    # Encode the image to base64 for embedding in HTML
+    category_plot = base64.b64encode(buffer.getvalue()).decode('utf8')
+    plt.close()
+
+    # Render the template with the enbedding plot
+    return render_template('feature2.html', category_plot=category_plot)
 
 @app.route('/feature3.html', methods=['GET'])
 def render_feature_3():
