2aacd3c5170dcd2c880875c0fdc3e8e6debb7ae0
Diego Bolaños <diego.bolanosvillalobos@ucr.ac.cr>
2024-08-25 17:22:42 -0600

    Fixed linter errors and added Jinja config to remove whitespace from final HTML.

--- TP1/Avance1/app/__init__.py
+++ TP1/Avance1/app/__init__.py
@@ -1,7 +1,13 @@
+"""
+Creates and configures flask app. Uses the factory design pattern.
+"""
 from flask import Flask
 
 def create_app():
+    """Returns a flask app"""
     app = Flask(__name__)
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
     with app.app_context():
         from . import routes
     return app
