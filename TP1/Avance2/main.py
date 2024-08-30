151a2865a9255ebeff84aaf84c8a68f36fc8853c
JosePabloMoraC <84686084+JosePabloMoraC@users.noreply.github.com>
2024-08-30 10:37:16 -0600

    Added example lexer

--- TP1/Avance2/main.py
+++ TP1/Avance2/main.py
@@ -1,7 +1,13 @@
-from lexer import proof
+from lexer import proof, tokenize
 
 def main():
     proof()
+    # test it out
+    data = '''
+    3 + 4 * 10
+    + -20 *2
+    '''
+    tokenize(data)
 
 
 if __name__ == "__main__":
