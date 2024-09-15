bb1198fcf3e63c800f6daa31cc198e1d8be4d249
JosePabloMoraC <84686084+JosePabloMoraC@users.noreply.github.com>
2024-09-15 11:33:09 -0600

    Added production rules p_body, p_health_topic_nt and p_attributes_health_topic.

--- TP1/Avance3/parser/__init__.py
+++ TP1/Avance3/parser/__init__.py
@@ -3,37 +3,51 @@ from lexer import tokens
 
 def p_medi_plus_xml(t):
     'medi_plus_xml : header body TAG_HEALTH_TOPICS_CLOSURE'
-    print(t[3])
+    print(f'[p_medi_plus_xml]: {t[3]}')
 
 def p_header(t):
     'header : header_version_encoding TAG_DOCTYPE tag_health_topics_nt'
-    print(t[2])
+    print(f'[p_header]: {t[2]}')
 
 def p_header_version_encoding(t):
     'header_version_encoding : TAG_XML ATTRIBUTE_VERSION TEXT_OF_ATTRIBUTE ATTRIBUTE_ENCODING TEXT_OF_ATTRIBUTE XML_TAG_CLOSURE'
-    print(f'{t[1]} + {t[2]} + {t[3]} + {t[4]} + {t[5]} + {t[6]}')
+    print(f'[p_header_version_encoding]: {t[1]} + {t[2]} + {t[3]} + {t[4]} + {t[5]} + {t[6]}')
     
 def p_tag_health_topics(t):
     'tag_health_topics_nt : TAG_HEALTH_TOPICS ATTRIBUTE_TOTAL NUMBER ATTRIBUTE_DATE_GENERATED DATE TIME TAG_CLOSURE'
-    print(f'{t[1]} + {t[2]} + {t[3]} + {t[4]} + {t[5]} + {t[6]} + {t[7]}')
+    print(f'[p_tag_health_topics]: {t[1]} + {t[2]} + {t[3]} + {t[4]} + {t[5]} + {t[6]} + {t[7]}')
 
 def p_body(t):
-    'body : empty'
-    #'''body : tag_health_topic_nt tags_under_health_topic TAG_HEALTH_TOPIC_CLOSURE body 
-     #       | empty'''
+    '''body : tag_health_topic_nt tags_under_health_topic TAG_HEALTH_TOPIC_CLOSURE body 
+            | empty'''
+    if(len(t) > 2):
+        print(f'[p_body]: {t[3]}')
+
+def p_health_topic_nt(t):
+    'tag_health_topic_nt : TAG_HEALTH_TOPIC attributes_health_topic TAG_CLOSURE'
+    print(f'[tag_health_topic_nt]: {t[1]} + {t[3]}') 
+
+def p_attributes_health_topic(t):
+    'attributes_health_topic : ATTRIBUTE_META_DESC TEXT_OF_ATTRIBUTE ATTRIBUTE_TITLE TEXT_OF_ATTRIBUTE ATTRIBUTE_URL URL ATTRIBUTE_ID NUMBER ATTRIBUTE_LANGUAGE TEXT_OF_ATTRIBUTE ATTRIBUTE_DATE_CREATED DATE'
+    print(f'[p_attributes_health_topic]: {t[1]} + {t[2]} + {t[3]} + {t[4]} + {t[5]} + {t[6]} + {t[7]} + {t[8]} + {t[9]} + {t[10]} + {t[11]} + {t[12]} + {t[6]} + {t[7]}')
+
+def p_tags_under_health_topic(t):
+    'tags_under_health_topic : empty' 
 
 """
 DONE:
 MEDI_PLUS_XML -> HEADER BODY tag-health-topics-closure
 HEADER -> header-version-encoding tag-doctype TAG-HEALTH-TOPICS  
 TAG-HEALTH-TOPICS -> tag-health-topics attribute-total number attribute-date-generated date time closure
+BODY -> TAG-HEALTH-TOPIC-NT TAGS-UNDER-HEALTH-TOPIC tag-health-topic-closure BODY | ɛ
+TAG-HEALTH-TOPIC-NT -> tag-health-topic ATTRIBUTES-HEALTH-TOPIC closure  
+ATTRIBUTES-HEALTH-TOPIC -> attribute-meta-desc text-of-attribute attribute-title text-of- attribute attribute-url url-text attribute-id number attribute-language text-of-attribute attribute-date-created date
 
 WORK_IN_PROGRESS:
-BODY -> TAG-HEALTH-TOPIC-NT TAGS-UNDER-HEALTH-TOPIC tag-health-topic-closure BODY | ɛ
+TAGS-UNDER-HEALTH-TOPIC -> TAG-ALSO-CALLED-NT TAG_LANGUAGE-MAPPED-TOPIC-NT tag-full-summary TAG-GROUP-NT TAG-SEE-REFERENCE-NT TAG-MESH-HEADING-NT TAG-RELATED-TOPIC-NT TAG-OTHER-LANGUAGE-NT TAG-PRIMARY-INSTITUTE-NT TAG-SITE-NT
+
 
 TODO:
-TAG-HEALTH-TOPIC-NT -> tag-health-topic ATTRIBUTES-HEALTH-TOPIC closure   
-ATTRIBUTES-HEALTH-TOPIC -> attribute-meta-desc text-of-attribute attribute-title text-of- attribute attribute-url url-text attribute-id number attribute-language text-of-attribute attribute-date-created date
 TAGS-UNDER-HEALTH-TOPIC -> TAG-ALSO-CALLED-NT TAG_LANGUAGE-MAPPED-TOPIC-NT tag-full-summary TAG-GROUP-NT TAG-SEE-REFERENCE-NT TAG-MESH-HEADING-NT TAG-RELATED-TOPIC-NT TAG-OTHER-LANGUAGE-NT TAG-PRIMARY-INSTITUTE-NT TAG-SITE-NT
 TAG-ALSO-CALLED-NT -> tag-also-called text-of-tag tag-also-called-closure TAG-ALSO-CALLED-NT | ɛ
 TAG_LANGUAGE-MAPPED-TOPIC-NT -> language-mapped-topic ATTRIBUTES-HEALTH-TOPIC closure text-of-tag language-mapped-topic-closure | ɛ
